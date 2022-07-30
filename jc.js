dc00799e38e295bd92e35774a45e76b92b34a8bf
Rishabh Tiwari <110122658+rishabht10@users.noreply.github.com>
2022-07-30 22:53:55 +0530

    disappearing navbar

--- jc.js
+++ jc.js
@@ -3,3 +3,16 @@ const subbtn = document.getElementById("q4");
         function prtSub() {
            alert("Query Submitted");
         }
+
+let cont=document.getElementById("divcont");
+
+
+cont.addEventListener("mouseover",()=>{
+   document.getElementById("navbr").classList.add("disno");
+
+})
+
+cont.addEventListener("mouseleave",()=>{
+
+   document.getElementById("navbr").classList.remove("disno");   
+})
